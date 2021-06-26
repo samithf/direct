d633baad129ee0af98797126fec17b499b7a157c
samithf <h.samithdilhara@gmail.com>
2021-06-26 22:42:24 +0530

    change page title place 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,12 @@
 import "../styles/global.css";
-import Head from "next/document";
 
 import Page from "../layouts/Page";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Free Video Meetings</title>
-      </Head>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
-    </>
+    <Page>
+      <Component {...pageProps} />
+    </Page>
   );
 }
 
