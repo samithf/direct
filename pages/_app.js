cee06c0a86b02b8e2efa556307ff28aebb1d73b7
samithf <h.samithdilhara@gmail.com>
2021-06-27 09:53:11 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,13 @@
 import React from "react";
-import Head from "next/head";
-
 import "../styles/global.css";
 
 import Page from "../layouts/Page";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Free Video Meetings</title>
-      </Head>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
-    </>
+    <Page>
+      <Component {...pageProps} />
+    </Page>
   );
 }
 
